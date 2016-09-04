1046fea1b1c38f9f52cc2304f72dc524301a783e
Jordan Farnworth <jordanfarn23@gmail.com>
2016-09-04 17:02:19 -0600

    fixed view for mobile

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
-
+var MobileDetect = require('mobile-detect');
 import Icon from './Icon.jsx';
 
 // App component - represents the whole app
 export default class App extends Component {
   componentDidMount() {
-    // debugger
+    detecter = new MobileDetect(window.navigator.userAgent);
+    isMobile = detecter.match('Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini')
+    if(isMobile){
+      $('video').hide()
+      $('body').css("background-image", "url(https://s3-us-west-2.amazonaws.com/videos-jordan/monitor-933392.jpg)");  
+    }
+  }
+
+  handleMobile() {
+    console.log("handling...");
   }
 
   listIcons() {
@@ -35,7 +44,7 @@ export default class App extends Component {
             <h1>Jordan Farnworth</h1>
             <img className="img-responsive center-block" id="main-img" src="https://s3-us-west-2.amazonaws.com/videos-jordan/me.jpg" ></img>
             <p>Full Stack Developer</p>
-            <p>at  <span><a id="inst-link" href="https://www.instructure.com/" target="_blank" >Instructure</a></span></p>
+            <p>at <span><a id="inst-link" href="https://www.instructure.com/" target="_blank" >Instructure</a></span></p>
             <p>farnworth.jordan@gmail.com</p>
             <div id="icons">
               {this.renderIcons()}
